051b7d5e5c347631a7a0dd554f47ff950311aab8
Gopal V <gopalv@apache.org>
2021-06-11 20:53:14 -0700

    python3 changes - cut 1

--- lipwig.py
+++ lipwig.py
@@ -9,6 +9,7 @@ from itertools import count as counter
 from itertools import chain
 from collections import defaultdict
 from math import log10
+from functools import reduce
 
 NX = True
 try:
@@ -34,10 +35,10 @@ def simple():
 	global SIMPLE
 	return SIMPLE
 
-nextInt = counter().next 
+nextInt = counter().__next__ 
 
 def ifseteq(h, k, v):
-	return h.has_key(k) and h[k] == v
+	return k in h and h[k] == v
 def lwrap(t, n=32):
 	return "\n".join(textwrap.wrap(t, n))
 
@@ -59,40 +60,40 @@ class TezEdge(object):
 		if self.srcV.dag.plan.counters:
 			ctrname="TaskCounter_%s_OUTPUT_%s" % (self.srcV.name.replace(" ","_"), self.dstV.name.replace(" ", "_"))
 			label = "%s" % (self.kind)
-			if (self.srcV.dag.plan.counters.has_key(ctrname)):
+			if (ctrname in self.srcV.dag.plan.counters):
 				edgectr = self.srcV.dag.plan.counters[ctrname]
-				if (edgectr.has_key(u'OUTPUT_BYTES_PHYSICAL')):
-					bytesout = edgectr[u'OUTPUT_BYTES_PHYSICAL']['counterValue']
+				if ('OUTPUT_BYTES_PHYSICAL' in edgectr):
+					bytesout = edgectr['OUTPUT_BYTES_PHYSICAL']['counterValue']
 					label = "%s (%s)" % (self.kind, size_fmt(int(bytesout))) 
 		(s,t,k) = self.srcV.dag.weights.edge2ops(self)
 		if self.srcV.dag.weights.iscriticalpath(s,t):
 			style = "color=red";
-		print '%s:s -> %s:%s [label="%s", weight=100, %s];' % (s,t,self.port, label, style)
+		print('%s:s -> %s:%s [label="%s", weight=100, %s];' % (s,t,self.port, label, style))
 	def claim(self, vmap, opmap):
 		self.srcV = vmap[self.src]
 		self.dstV = vmap[self.dst]
 		srcops = vmap[self.src].opset
 		dstops = vmap[self.dst].opset
-		for op in srcops.values():
-			if op.has_key("Target Vertex:") and op["Target Vertex:"] == self.dst:
+		for op in list(srcops.values()):
+			if "Target Vertex:" in op and op["Target Vertex:"] == self.dst:
 				if self.kind == 'DPP' and op["OperatorId:"].startswith("EVENT_"):
 					self.srcOp = op
 					return
-			if op.has_key("outputname:") and op["outputname:"] == self.dst:
+			if "outputname:" in op and op["outputname:"] == self.dst:
 				self.srcOp = op
-			if (op.has_key('outputOperator:')):
+			if ('outputOperator:' in op):
 				# another 1-1 assumption
 				outop = op['outputOperator:'][0]
-				if dstops.has_key(outop):
+				if outop in dstops:
 					self.srcOp = op
 					self.dstOp = dstops[outop]
-					if (self.dstOp.has_key('input vertices:')):
+					if ('input vertices:' in self.dstOp):
 						inputs = set(self.dstOp['input vertices:'].values())
 						if (self.src in inputs):
 							self.port = 'e'
 						# do not trust it - no return
-		for op in dstops.values():
-			if (op.has_key('input vertices:')):
+		for op in list(dstops.values()):
+			if ('input vertices:' in op):
 				inputs = set(op['input vertices:'].values())
 				if op != self.dstOp and self.dstOp:
 					comment("broken explain for " + self.srcOp['OperatorId:'] + " -> " + self.dstOp['OperatorId:']);
@@ -101,13 +102,13 @@ class TezEdge(object):
 					self.port = 'e'
 					return
 		if self.kind == "CONTAINS":
-			for op in srcops.values():
-				if (op.has_key('outputOperator:')):
+			for op in list(srcops.values()):
+				if ('outputOperator:' in op):
 					# one level deeper
 					outop = op['outputOperator:'][0]
-					if opmap.has_key(outop):
+					if outop in opmap:
 						finalop = opmap[outop]
-						if finalop.has_key("input vertices:"):
+						if "input vertices:" in finalop:
 							inputs = set(finalop['input vertices:'].values())
 							if self.dst in inputs:
 								self.srcOp = op
@@ -137,11 +138,12 @@ class TezVertex(object):
 		self.empty = True
 		self.prefix = name.replace(" ", "_")
 		self.events = []
+		self.tree = {}
 		self.critical = False
 		for k in raw:
 			if k == "Execution mode:":
 				self.vectorized = "vectorized" in raw[k]
-			elif k.find("Operator Tree"):
+			elif "Operator Tree" in k:
 				self.empty = False
 				self.tree = raw[k]
 				# annoying details Map operator uses a list
@@ -163,14 +165,14 @@ class TezVertex(object):
 	def getops(self, ops):
 		if type(ops) is not dict:
 			return
-		for (k,v) in ops.items():
-			if v.items():
-				if v.has_key('OperatorId:'):
+		for (k,v) in list(ops.items()):
+			if list(v.items()):
+				if 'OperatorId:' in v:
 					yield (v['OperatorId:'],v)
 				else:
 					v['OperatorId:'] = "FAKE_%d" % (nextInt())
 					yield (v['OperatorId:'],v)
-			for k1,v1 in v.items():
+			for k1,v1 in list(v.items()):
 				if (k1 == "children" and v1): 
 					if type(v1) is list:
 						for v2 in v1:
@@ -196,11 +198,11 @@ class TezVertex(object):
 	def draw(self):
 		self.nodes = 0
 		color = "blue" if self.vectorized else "red"
-		print "subgraph cluster_%s {" % self.prefix 
-		print 'style=dashed ;' 
-		print "compound=true;"
-		print "rank=same;"
-		print "color=%s;" % color
+		print("subgraph cluster_%s {" % self.prefix) 
+		print('style=dashed ;') 
+		print("compound=true;")
+		print("rank=same;")
+		print("color=%s;" % color)
 		opts = ["vectorized=%s" % str(self.vectorized).lower()]
 		t = self.timing()
 		if t:
@@ -208,18 +210,18 @@ class TezVertex(object):
 			opts.append("own time=%d ms" % (e1-s2))
 			if (s1 != s2):
 				opts.append("waiting+= %d ms" % (s2-s1)) 
-		print 'label="%s\\n (%s)";' % (self.name, ", ".join(opts))
+		print('label="%s\\n (%s)";' % (self.name, ", ".join(opts)))
 		self.drawOp(self.tree, None, None)
-		print "}"
+		print("}")
 
 	def op2id(self, op):
-		for (k,v) in op.items():
-			if (v.has_key("OperatorId:")):
+		for (k,v) in list(op.items()):
+			if ("OperatorId:" in v):
 				return v["OperatorId:"] 
 	def op2edges(self):
 		for opid in self.opset:
 			op = self.opset[opid]
-			if (op.has_key("children")):
+			if ("children" in op):
 				c = op["children"]
 				if type(c) is list:
 					for c1 in c:
@@ -240,7 +242,7 @@ class TezVertex(object):
 		if type(ops) is not dict:
 			return
 		children = None
-		for (k,v) in ops.items():
+		for (k,v) in list(ops.items()):
 			nodeid = self.nodes
 			name = "%s" % (v['OperatorId:'])
 			self.nodes += 1
@@ -248,9 +250,9 @@ class TezVertex(object):
 				style = ""
 				if self.dag.weights.iscriticalpath(parent, name):
 					style="color=red"
-				print '%s -> %s [weight=1, label="%s" %s];' % (parent, name, prevstats, style) 
+				print('%s -> %s [weight=1, label="%s" %s];' % (parent, name, prevstats, style)) 
 			text = ["<tr><td colspan=\"2\"><b>%s</b></td></tr>" % k]
-			for k1,v1 in v.items():
+			for k1,v1 in list(v.items()):
 				if (k1 == "children" and v1): 
 				    children = v1
 				elif k1 == "Statistics:":
@@ -258,7 +260,7 @@ class TezVertex(object):
 					rawsize = v1[v1.find("Data size:")+len("Data size:") : v1.find("Basic ")]
 					if self.dag.plan.counters:
 						hivectrs = self.dag.plan.counters["HIVE"]
-						if (hivectrs.has_key("RECORDS_OUT_OPERATOR_%s" % name)):
+						if ("RECORDS_OUT_OPERATOR_%s" % name in hivectrs):
 							rctr = hivectrs["RECORDS_OUT_OPERATOR_%s" % name]["counterValue"]
 						else:
 							rctr = -1
@@ -285,10 +287,10 @@ class TezVertex(object):
 						self.drawOp(v2, name, currstats)
 				else:
 					self.drawOp(children, name, currstats)
-			if v.items():
-				print '%s [shape=plaintext,label=<%s>];' % (name, "<table>%s</table>" % "\n".join(text)) 
+			if list(v.items()):
+				print('%s [shape=plaintext,label=<%s>];' % (name, "<table>%s</table>" % "\n".join(text))) 
 			else:
-				print '%s [label=<%s>];' % (name, k) 
+				print('%s [label=<%s>];' % (name, k)) 
 
 class Op2Graph(object):
 	def __init__(self, dag):
@@ -334,7 +336,7 @@ class Op2Graph(object):
 				(s1, s2, e1) = self.dag.vmap[v.name].timing()
 				return (e1-s2)
 			timings = dict([(v.name, timing(v)) for v in self.dag.vertices])
-			vecops = lambda (v): [(op,v.name) for op in v.opset.keys()] 
+			vecops = lambda v: [(op,v.name) for op in list(v.opset.keys())] 
 			op2vx = dict(chain(*[vecops(v) for v in self.dag.vertices]))
 			comment(op2vx)
 			for (s,e) in [(x,y) for x in starts for y in ends]:
@@ -355,11 +357,11 @@ class HiveTezDag(object):
 		self.plan = plan
 		self.query = q
 		self.name = raw.get("DagName:") or raw.get("DagId:") or "Unknown"
-		self.edges = reduce(lambda a,b: a+b, [list(TezEdge.create(k,v)) for (k,v) in ((raw.has_key("Edges:") and raw["Edges:"]) or {}).items()], [])
-		self.vertices = [TezVertex(self, k,v) for (k,v) in raw["Vertices:"].items()]
+		self.edges = reduce(lambda a,b: a+b, [list(TezEdge.create(k,v)) for (k,v) in list((("Edges:" in raw and raw["Edges:"]) or {}).items())], [])
+		self.vertices = [TezVertex(self, k,v) for (k,v) in list(raw["Vertices:"].items())]
 		self.vmap = dict([(v.name, v) for v in self.vertices])
 		opmap = reduce(lambda a,b: a.update(b) or a, [v.opset for v in self.vertices], {})
-		comment(opmap.keys())
+		comment(list(opmap.keys()))
 		for v in self.vertices:
 			for k in v.opset:
 				if (k.startswith("EVENT_")):
@@ -374,7 +376,7 @@ class HiveTezDag(object):
 		return [self.vmap[v] for v in self.weights.parents(vname)]
 	def vevents(self, evs):
 		for v in self.vertices:
-			if (evs.has_key(v.name)):
+			if (v.name in evs):
 				v.events = evs[v.name]
 		self.weights.compute()
 	def draw(self):
@@ -384,21 +386,21 @@ class HiveTezDag(object):
 class HivePlan(object):
 	def __init__(self, q, raw):
 		self.raw = raw
-		stages = [(k,HiveTezDag(self, q, v)) for (k,v) in raw["STAGE PLANS"].items() if v.has_key("Tez")]
+		stages = [(k,HiveTezDag(self, q, v)) for (k,v) in list(raw["STAGE PLANS"].items()) if "Tez" in v]
 		assert len(stages) == 1
 		self.stages = stages.pop()
 		self.counters = {} # none for "explain formatted" 
 	def vevents(self, vevents):
 		self.stages[1].vevents(vevents)
 	def draw(self):
-		print "digraph g {"
-		print "node [shape=box];"
-		print 'node [id="\N"];'
-		print 'compound=true;'
+		print("digraph g {")
+		print("node [shape=box];")
+		print('node [id="\\N"];')
+		print('compound=true;')
 		#print 'splines=ortho;'
-		print ""
+		print("")
 		self.stages[1].draw()
-		print "}"
+		print("}")
 
 def findOneOfThem(nameslist, lookingfor):
 	l = [value for value in lookingfor if value in nameslist]
@@ -412,7 +414,7 @@ def openPackage(f):
 			jname = findOneOfThem(zz.namelist(), ['DAS/QUERY.json', 'QUERY.json'])
 			if not jname:
 				# this will throw an error, but we want one
-				print 'File contains: ', zz.namelist()
+				print('File contains: ', zz.namelist())
 				jname = "DAS/QUERY.json"
 			qdata = json.loads(zz.read(jname))
 			query = qdata['query']
@@ -430,7 +432,7 @@ def openPackage(f):
 				dagDetails = vdata["dag"]["dagDetails"]
 				details = dagDetails
 			# details might come out of DAG details
-			if details.has_key('counters') and details['counters']:
+			if 'counters' in details and details['counters']:
 				countergroups = dict([(c['counterGroupName'], dict([(x["counterName"],x) for x in c['counters']])) for c in details['counters']]) 
 				plan.counters = countergroups
 			return plan
